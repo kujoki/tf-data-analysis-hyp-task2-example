eac81d9774a7d8bd061d39613fd36b6f1a5382af
Kate <90715013+kujoki@users.noreply.github.com>
2023-04-13 17:30:02 +0300

    Попытка №3

--- solution.py
+++ solution.py
@@ -6,7 +6,7 @@ from scipy.stats import ks_2samp
 
 chat_id = 670223087 # Ваш chat ID, не меняйте название переменной
 
-def test_randomizer(x: np.array, y: np.array):
+def solution(x: np.array, y: np.array):
     alpha=0.03
     n = len(x)
     critical_value = 1.36 / np.sqrt(n)
